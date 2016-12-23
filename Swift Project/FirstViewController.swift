bebd0adb7882d6e5b13fe3a39d8758c7225ad767
jackzumwalt <jack.zumwalt@hotmail.com>
2016-12-22 23:13:47 -0600

    Initial Commit

---
+++ Swift Project/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Swift Project
+//
+//  Created by Jack Zumwalt on 12/22/16.
+//  Copyright © 2016 Jack Zumwalt. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
